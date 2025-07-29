a1dd383a8d48633d9d727f96075aad8503935493
easly1989 <caesar.themaster@gmail.com>
2025-07-29 10:43:36 +0200

    Refactor extract_errors function to return Vec<&str> and update main to handle errors correctly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
-fn extract_errors(text: &str) -> Vec<String> {
+fn extract_errors(text: &str) -> Vec<&str> {
     let split = text.split('\n');
     let mut results = Vec::new();
     for line in split {
         if line.starts_with("ERROR") {
-            results.push(line.to_string());
+            results.push(line);
         }
     }
 
@@ -13,17 +13,22 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    let mut errors = Vec::new();
-
     match fs::read_to_string("logs.txt") {
         Ok(content) => {
-            errors = extract_errors(content.as_str());
-
+            let errors = extract_errors(content.as_str());
+            match fs::write("errors.txt", errors.join("\n")) {
+                Ok(_) => {
+                    println!("Errors extracted successfully to errors.txt");
+                }
+                Err(e) => {
+                    eprintln!("Failed to write errors to file: {}", e);
+                }
+            }
         }
         Err(e) => {
             eprintln!("Failed to read file: {}", e);
         }
     }
 
-    println!("{:#?}", errors);
+    
 }
