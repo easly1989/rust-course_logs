d5502629cf92d014ed8bf1dc4195bb5be9a8e946
easly1989 <caesar.themaster@gmail.com>
2025-07-29 10:33:17 +0200

    Refactor error extraction into a separate function and update main to handle file reading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 use std::fs;
 
-fn main() {
-    let text = fs::read_to_string("logs.txt");
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split = text.split('\n');
+    let mut results = Vec::new();
+    for line in split {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
 
-    println!("File content: {:#?}", text);
+    results
+}
+
+fn main() {
+    match fs::read_to_string("logs.txt") {
+        Ok(content) => {
+            let errors = extract_errors(content.as_str());
+            println!("{:#?}", errors);
+        }
+        Err(e) => {
+            eprintln!("Failed to read file: {}", e);
+        }
+    }
+
+    
 }
